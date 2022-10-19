b1d803781b885698c8d1a44f6eca8057d8257417
mdrazu2543 <alif.pab120na.12@gmail.com>
2022-10-19 15:56:06 +0600

    changes

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,19 @@
+@php
+
+ $routeName = \Request::route()->uri
+
+@endphp
+
+
+
+<header class="h-[56px] w-full flex border items-center px-10 justify-between ">
+    <div>
+        <h1 class="font-extrabold text-[20px]">E-commemrce</h1>
+    </div>
+    <div class="flex gap-5 text-gray-600">
+        <h4  class="text-red-500 font-bold hover:scale-105 transition-alls"><a href="/">Home</a></h4>
+        <h4><a href="/">Login</a></h4>
+        <h4><a href="/">Register</a></h4>
+
+    </div>
+</header>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'E-commerce') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -17,23 +17,16 @@
         @livewireStyles
     </head>
     <body class="font-sans antialiased">
-        <x-jet-banner />
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
 
-            <!-- Page Content -->
+        @section("header")
+        <x-header/>
+        @show
+
+        <div class="min-h-screen bg-gray-100 px-4 ">
             <main>
-                {{ $slot }}
+               @yield('content')
             </main>
         </div>
 
